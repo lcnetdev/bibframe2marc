b1e38b671d7e16a65906d06eb051f142fd16aa07
Kevin Ford <kefo@loc.gov>
2022-08-05 14:18:48 -0400

    Changes to and additions to exemplar resources.

--- rules/test/data/exemplars/fetch_exemplars.sh
+++ rules/test/data/exemplars/fetch_exemplars.sh
@@ -10,6 +10,12 @@ declare -A exemplars=(
 ["10496140"]="notatedmusic-manuscript-monograph-01"
 ["20525980"]="notatedmusic-manuscript-monograph-02"
 
+["21009588"]="musicaudio-monograph-varianttitles-subjects-008p"
+["11457859"]="text-serial-SUBJECTS-relationships-008c"
+["11393548"]="text-serial-008d"
+["14437118"]="stillimage-collection-pnp-008i"
+["12280298"]="musicaudio-monograph-contributions-008m"
+["22724623"]="stillimage-monograph-subjects-pnp-008q"
 )
 
 for key in "${!exemplars[@]}"; do
